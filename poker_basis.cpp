d7c437903de14c7200fdc802984ed9a65af25d4f
KevinJeongJ <jihyuk.jeong@mail.mcgill.ca>
2018-12-10 00:29:27 -0500

    winning hand caclulation is added, still need to identify the winning hand with the same strength (ex, pair vs pair)

--- poker_basis.cpp
+++ poker_basis.cpp
@@ -4,17 +4,17 @@ shorten the code significantly.
 calculation of the pot (how much each people bet, raise mechanics,etc) and
 winning hand calculation is not made yet. 
 */
-
-//edit: Winner function is added into the build to calculate the winning hand
-// royal flush and straight flush has not been implemented yet
-
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <stdio.h>
 #include <math.h>
+#include <cmath>
+#include <vector>
+#include <cstring>
 using namespace std;
 
-int** calc_point(double dummycards[][10], double dummycommon[], int people);
+int calc_point(double dummycards[][10], double dummycommon[5], int people);
 
 int main()
 {
@@ -47,8 +47,8 @@ int main()
     int c1;
     int cd1;
 
-    for (int d = 1; d <= pe; d++)
-        for (int e = 1; e <= 2; e++)
+    for (int d = 0; d < pe; d++)
+        for (int e = 0; e < 2; e++)
         {
             // randomly drawing cards
             c1 = rand() % 16 + 1;
@@ -74,10 +74,10 @@ already taken */
         }
 
     //outputting cards each player obtained
-    for (int d = 1; d <= pe; d++)
+    for (int d = 0; d < pe; d++)
     {
         cout << "player " << d << " has following cards" << endl;
-        for (int e = 1; e <= 2; e++)
+        for (int e = 0; e < 2; e++)
         {
             cout << cards[e][d] << endl;
         }
@@ -88,7 +88,7 @@ already taken */
     int cc1;
     int ccd1;
 
-    for (int i = 1; i <= 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         cc1 = rand() % 16 + 1;
         ccd1 = rand() % 4 + 1;
@@ -110,7 +110,7 @@ already taken */
 
     //printing first 3 common cards
     cout << "First 3 common cards are:" << endl;
-    for (int i = 1; i <= 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         cout << commoncards[i] << ".\n"
              << endl;
@@ -128,7 +128,7 @@ already taken */
         return 0;
     }
 
-    i = 4;
+    i = 3;
     {
         cc1 = rand() % 16 + 1;
         ccd1 = rand() % 4 + 1;
@@ -150,7 +150,7 @@ already taken */
 
     //printing first 3 common cards
     cout << "4th common card is:" << endl;
-    i = 4;
+    i = 3;
     {
         cout << commoncards[i] << ".\n"
              << endl;
@@ -166,7 +166,7 @@ already taken */
     }
 
     //last card
-    i = 5;
+    i = 4;
     {
         cc1 = rand() % 16 + 1;
         ccd1 = rand() % 4 + 1;
@@ -188,7 +188,7 @@ already taken */
 
     //printing first 3 common cards
     cout << "last card is:" << endl;
-    i = 5;
+    i = 4;
     {
         cout << commoncards[i] << ".\n"
              << endl;
@@ -205,9 +205,14 @@ already taken */
 
     // funciton to caclualte the point
 
-    int** win = calc_point(cards, commoncards, pe);
+    int win = calc_point(cards, commoncards, pe);
 
-    cout << win <<".\n";
+   if(win > pe){
+        cout << "It's a TIE";
+        return 0;
+    }
+
+    cout << "winner is Player: " << win ;
 
     return 0;   
 }
@@ -218,15 +223,15 @@ already taken */
 
 //function to calculate the points
 // should be noted that only 10 maximum players can play
-int** calc_point(double dummycards[][10], double dummycommon[], int people)
+int calc_point(double dummycards[][10], double dummycommon[5], int people)
 {
     double points[people];
 
     // including common cards into the 7cards for each person calculation
     // two personal + five common cards for calculation
-    for (int j = 1; j <= people; j++)
+    for (int j = 0; j <= people; j++)
     {
-        for (int k = 3; k <= 7; k++)
+        for (int k = 2; k <= 6; k++)
         {
             dummycards[k][j] = dummycommon[k - 2];
         }
@@ -244,22 +249,23 @@ two pair = 3
 pair = 2
 high card = 1 */
 
-    for (int i = 1; i <= people; i++)
+    for (int i = 0; i < people; i++)
     {
         int count = 0;
         int count_straight = 0;
-        for (int d = 1; d <= 7; d++)
-            for (int e = 1; e <= 7; e++)
+
+        for (int d = 0; d < 7; d++)
+            for (int e = 0; e < 7; e++)
             {
 
                 // looking for pairs
-                if (e > d || e < d)
-                {
-                    if ((dummycards[d][i] - dummycards[e][i]) < 1)
+                if (e != d)
+                
+                    if (abs((dummycards[d][i] - dummycards[e][i])) < 0.5)
                     {
                         count++;
                     }
-                }
+                
             }
         // mathematically, for a single pair, count =2
         // for two pair, count = 4
@@ -293,15 +299,14 @@ high card = 1 */
 
         //search for straight
         double indi[7];
-        for (int de = 1; de <= 7; de++)
+        for (int de = 0; de < 7; de++)
         {
             indi[de] = dummycards[de][i];
         }
 
-        size_t size = sizeof(indi) / sizeof(indi[0]);
-        sort(indi, indi + size);
-        for (int n = 1; n <= 3; n++)
-            for (int m = n; m <= 4; m++)
+       sort(begin(indi), end(indi));
+        for (int n = 0; n < 3; n++)
+            for (int m = n; m < 4; m++)
             {
                 if (indi[m + 1] - indi[m] < 1)
                 {
@@ -320,7 +325,7 @@ high card = 1 */
         int count_2 = 0;
         int count_3 = 0;
         int count_4 = 0;
-        for (int b = 1; b <= 7; b++)
+        for (int b = 0; b < 7; b++)
         {
             if (fmod((indi[b] * 10), 10) == 1)
             {
@@ -346,16 +351,18 @@ high card = 1 */
     }
 
     //calculate winner
-    int** winner = 0;
-    for (int je = 1; je <= people; je++)
+    int winner; 
+
+    for (int je = 0; je < people; je++)
     {
-        for (int pe = 1; pe <= people; pe++)
-        {
-            if (points[je] > points[pe])
-            {
-                **winner = je;
+        for(int de = 0; de<people;de++){
+            if (points[de]>points[je])
+            if (de != je){
+                winner = de;
+            }
             }
         }
-    }
-    return winner;
+    
+
+    return winner; 
 }
